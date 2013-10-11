c322fd93a3b812bd7c636eda443b9986b4371f97
Simone Giertz <simone@giertz.com>
2013-10-11 11:37:26 +0200

    Fixed function that returns distance between coordinates in km.

--- js/main.js
+++ js/main.js
@@ -36,17 +36,21 @@ function showError(error)
 getLocation();
 });
 
-function calculateDistance(lat1, long1, lat2, long2) {
-    // Translate to a distance
-    var distance =
-      Math.sin(lat1 * Math.PI) * Math.sin(lat2 * Math.PI) +
-      Math.cos(lat1 * Math.PI) * Math.cos(lat2 * Math.PI) * Math.cos(Math.abs(long1 - long2) * Math.PI);
-      console.log(distance);
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
+}
 
-    // Return the distance in miles
-    //return Math.acos(distance) * 3958.754;
-
-    // Return the distance in meters
-    return Math.acos(distance) * 6370981.162;
-} // CalculateDistance
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
 
