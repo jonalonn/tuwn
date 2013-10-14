631ea7219131738fdea2af915543ff5c88265a3b
Simone Giertz <simone@giertz.com>
2013-10-14 11:54:40 +0200

    upprensning av funktioner och testversion av att kolla om devicen har samma position som en variabel

--- js/main.js
+++ js/main.js
@@ -1,15 +1,45 @@
 $( document ).ready(function() {
-
 });
+
+var testCoordinates=[17.9919036,59.2985901]
+
+};
+
+var int=self.setInterval(function(){getLocation()},1000);
+var currentPosition=[];
+var previousPosition=[];
+
+function getLocation()
+{  
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+function showPosition(position)
+{
+ document.getElementById('longval2').value=position.coords.longitude;   
+ document.getElementById('latval2').value=position.coords.latitude; 
+ currentPosition=[position.coords.longitude,position.coords.latitude]
+ if(currentPosition==testCoordinates){
+    document.getElementById('result').value="DET FUNKAR"; 
+ }
+}
+
+
+$(document).on("mouseover", "#images img", function() {
+  var imageClass=$(this).attr("class");
+  var audio=new Audio("wood/wood"+imageClass+".ogg");;
+  audio.play();
+});
+
 function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
   var R = 6371; // Radius of the earth in km
   var dLat = deg2rad(lat2-lat1);  // deg2rad below
   var dLon = deg2rad(lon2-lon1); 
   var a = 
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ; 
+  Math.sin(dLat/2) * Math.sin(dLat/2) +
+  Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+  Math.sin(dLon/2) * Math.sin(dLon/2)
+  ; 
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
   var d = R * c; // Distance in km
   return d;
@@ -18,48 +48,3 @@ function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
 function deg2rad(deg) {
   return deg * (Math.PI/180)
 }
-if(navigator.geolocation){
-
-        var wpid = navigator.geolocation.watchPosition(success, error, {
-            enableHighAccuracy: true, 
-            maximumAge: 100, 
-            timeout: 10000
-        });
-
-        function success(position){
-          console.log("hej")
-            console.log(position.coords.longitude+","+position.coords.latitude)
-        }
-
-        function error(error){
-            switch(error.code){
-                case 1:
-                    alert('permission denied');
-                    break;
-                case 2:
-                    alert('position unavailable');
-                    break;
-                case 3:
-                    alert('timeout');
-                    break;
-                default:
-                    alert('unknown error');
-                    break;
-            }
-        }
-    }
-
-
-    var int=self.setInterval(function(){getLocation()},1000);
-function getLocation()
-  {
-  
-    navigator.geolocation.getCurrentPosition(showPosition);
-    
-  
-  }
-function showPosition(position)
-  {
- document.getElementById('longval2').value=position.coords.longitude;   
- document.getElementById('latval2').value=position.coords.latitude; 
-  }
