5617a4f0f665f38afda0e8d52b1b1d318642fe05
Simone Giertz <simone@giertz.com>
2013-10-23 15:52:57 +0200

    new icons

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ function showPosition(position)
   myLatLng = new google.maps.LatLng(currentPosition[0], currentPosition[1]);
   if(mapOnSite){
     map.setCenter(myLatLng)
-    var image = new google.maps.MarkerImage(
+    var userMarkerImage = new google.maps.MarkerImage(
       'images/bluedot.png',
               null, // size
               null, // origin
@@ -37,7 +37,7 @@ function showPosition(position)
             // then create the new marker
             myMarker = new google.maps.Marker({
               flat: true,
-              icon: image,
+              icon: userMarkerImage,
               map: map,
               optimized: false,
               position: myLatLng,
@@ -145,9 +145,19 @@ service.nearbySearch(request, function(results,status,pagination){
     pagination.nextPage();
   }
 }); */
-
+var locationMarkerImage;
       for (var i = 0; i < csvResults.length; i++) {
       if(csvResults[i][1]){
+
+locationMarkerImage = new google.maps.MarkerImage(
+            'images/circle'+csvResults[i][0]+'.png',
+            null, // size
+            null, // origin
+            new google.maps.Point( 8, 8 ), // anchor (move to center of marker)
+            new google.maps.Size( 50, 50 ) // scaled size (required for Retina display icon)
+            )
+
+
         var marker = new google.maps.Marker(
         {
           position: new google.maps.LatLng(csvResults[i][2],csvResults[i][1]),
@@ -155,7 +165,7 @@ service.nearbySearch(request, function(results,status,pagination){
           flat: true, 
           optimized: false,
           title: csvResults[i][0],
-          icon: '../images/cat.gif'
+          icon: locationMarkerImage
         }
         )
         allMarkers.push(marker);
@@ -227,7 +237,6 @@ service.nearbySearch(request, function(results,status,pagination){
       }
     }
 
-console.log(amountOfMarkersWithAnIndex)
 }
   }
 
@@ -235,25 +244,6 @@ console.log(amountOfMarkersWithAnIndex)
 
 
 
-
-/*function getDistance(lat1,lon1,lat2,lon2) {
-  var R = 6371; // Radius of the earth in km
-  var dLat = deg2rad(lat2-lat1);  // deg2rad below
-  var dLon = deg2rad(lon2-lon1); 
-  var a = 
-  Math.sin(dLat/2) * Math.sin(dLat/2) +
-  Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-  Math.sin(dLon/2) * Math.sin(dLon/2)
-  ; 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c*1000; // Distance in m
-  //console.log(d)
-  return d;
-}
-
-function deg2rad(deg) {
-  return deg * (Math.PI/180)
-}*/
 
 //function initialize(userPosition) {
 //  mapOnSite=false
